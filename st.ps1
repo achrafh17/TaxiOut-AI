46d21fb65861f3c3a9a7ff322ba50deb8a86ae21
Mohamed Taha Aboumehdi Hassani <mtaha-sec@users.noreply.github.com>
2026-05-16 19:21:14 +0100

    DL integration

--- st.ps1
+++
@@ -1,7 +0,0 @@
-mkdir models
-mkdir reports
-
-New-Item notebooks\05_dl_preprocessing.ipynb -ItemType File
-New-Item notebooks\06_mlp_baseline.ipynb -ItemType File
-New-Item notebooks\07_dl_evaluation.ipynb -ItemType File
-New-Item notebooks\08_ml_vs_dl_comparison.ipynb -ItemType File
